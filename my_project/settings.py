5db6ec87279f3b2def6c9949a7b3e078d6ad14d3
beechy-92 <ashbeech39@gmail.com>
2025-10-25 13:56:13 +0100

    Add: heroku setup and connected

--- my_project/settings.py
+++ my_project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-=-)mp_r5*(4qmdb9myo61q#4jm=$vy_9eic-5wnky9lgw!b1&k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [' .herokuapp.com', '127.0.0.1', ]
 
 
 # Application definition
