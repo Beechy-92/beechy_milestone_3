4fdfbfe530e8ce69261cf167bc3ded2d01885e18
beechy-92 <ashbeech39@gmail.com>
2025-11-02 12:43:42 +0000

    add - slug_field and slug_url_kwarg to detail, update, delete and views

--- still_mind/views.py
+++ still_mind/views.py
@@ -12,13 +12,15 @@ class PostListView(ListView):
     model = Post
     paginate_by = 5
     template_name = "still_mind/post_list.html"
-    
+
     def get_queryset(self):
         return Post.objects.filter(status=1).order_by("-created_at")
 
 class PostDetailView(DetailView):
     model = Post
     template_name = "still_mind/post_detail.html"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
 
 class AuthorRequiredMixin(UserPassesTestMixin):
     def test_func(self):
@@ -38,9 +40,13 @@ class PostUpdateView(LoginRequiredMixin, AuthorRequiredMixin, UpdateView):
     model = Post
     form_class = PostForm
     template_name = "still_mind/post_form.html"
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
 
 class PostDeleteView(LoginRequiredMixin, AuthorRequiredMixin, DeleteView):
     model = Post
+    slug_field = "slug"
+    slug_url_kwarg = "slug"
     success_url = reverse_lazy("post_list")
     template_name = "still_mind/post_confirm_delete.html"
 
